c960d3583e98e00202cc3aeda86bd6b705a2d90e
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-05-21 22:58:18 +0300

    task10 First part

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const numberEl = document.querySelector("input")
+console.log(numberEl);
+const createEl = document.querySelector("button[data-create]")
+console.log(createEl);
+const destroyEl = document.querySelector("button[data-destroy]")
+console.log(destroyEl);
+
+
+
+
+createEl.addEventListener ("click", createBoxes)
+function createBoxes (){
+  const listEl = document.querySelector("#boxes")
+  const boxEl = document.createElement("div");
+  boxEl.classList.add("classJS");
+  boxEl.style.width = "30px";
+  boxEl.style.height = "30px";
+  boxEl.style.backgroundColor = getRandomHexColor();
+  listEl.append(boxEl);
+ 
+}
+
+destroyEl.addEventListener ("click", destroyBoxes)
+function destroyBoxes (){
+  const listEl = document.querySelector(".classJS")
+  listEl.remove()
+}
